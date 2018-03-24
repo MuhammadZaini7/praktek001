b7296a91adfd36500074b179ff4dc3d1506cdd8f
MuhammadZaini7 <32381402+MuhammadZaini7@users.noreply.github.com>
2018-03-24 16:14:48 +0800

    Mengisi Nilai Atribut

--- src/praktek001/Mie_instanAksi.java
+++ src/praktek001/Mie_instanAksi.java
@@ -15,6 +15,22 @@ public class Mie_instanAksi {
         mie_instan mie_kering = new mie_instan();
         mie_instan mie_kuah = new mie_instan();
         
+        mie_goreng.bumbu="Bawang Merah, Bawang Putih ";
+        mie_goreng.kecap="Kedelai Hitam";
+        mie_goreng.kemasan="Putih";
+        mie_goreng.minyak="Minyak Goreng";
+        mie_goreng.harga=5000;
         
+        mie_kering.bumbu="Bawang, Tomat ";
+        mie_kering.kecap="Kecap Sedap";
+        mie_kering.kemasan="Merah";
+        mie_kering.minyak="Minyak Lemak";
+        mie_kering.harga=3000;
+        
+        mie_kuah.bumbu="Sayur, Tomat ";
+        mie_kuah.kecap="Kecap Bango";
+        mie_kuah.kemasan="Biru";
+        mie_kuah.minyak="Minyak Lemak";
+        mie_kuah.harga=7000;
     }
 }
